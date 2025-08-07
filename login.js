f4663d74d77e43dcbf304242c47ab188ae1c6ce4
SkidBoi <kenzokay.business@gmail.com>
2025-08-07 23:57:08 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,167 @@
-document.getElementById("login-button").addEventListener("click", function () {
-    const user_number_input_____value = document.getElementById("user-number-input").value;
-    fetch("/.netlify/functions/getUserData?number=" + user_number_input_____value).then(fetch_____result => fetch_____result.json()).then(result_____json => {
-        if (result_____json && !result_____json.msg) {
-            localStorage.setItem("userData", JSON.stringify(result_____json));
-            window.location.href = "index.html";
-        } else {
-            alert("Fel kod!"); // Translates to: 'Wrong authentication key!'
+(function (_0x14c1a1, _0x4a367c) {
+  const _0x4a6f23 = _0x14c1a1();
+  while (true) {
+    try {
+      const _0xe0fb6a = -parseInt(_0x5ec3(408, 0x25e)) / 1 + -parseInt(_0x5ec3(383, 0x349)) / 2 + -parseInt(_0x5ec3(422, 0x283)) / 3 + -parseInt(_0x5ec3(373, 0x23f)) / 4 + -parseInt(_0x5ec3(424, 0x26b)) / 5 + -parseInt(_0x5ec3(423, 0x381)) / 6 + parseInt(_0x5ec3(403, 0x352)) / 7;
+      if (_0xe0fb6a === _0x4a367c) {
+        break;
+      } else {
+        _0x4a6f23.push(_0x4a6f23.shift());
+      }
+    } catch (_0x9737d4) {
+      _0x4a6f23.push(_0x4a6f23.shift());
+    }
+  }
+})(_0x1ddd, 961607);
+function _0x1ddd() {
+  const _0x31f18e = ['AxrLmZGYotm4mG', 'y3rVCIGICMv0Dq', 'Ahr0Chm6lY9KAq', 'C2vHCMnO', 'DxnLCKrHDge', 'Bg9JyxrPB24', 'DxnLCI1UDw1Izq', 'qw4GzxjYB3iGBW', 'mtCXota0mhHqsgrICG', 'DhjHy2u', 'zxjYB3i', 'qNLjza', 'rxjYB3i6', 'ihrVigzLDgnOia', 'CMv0DxjUicHMDq', 'DgHLBG', 'C3rLBMvY', 'BgvUz3rO', 'ywrKrxzLBNrmAq', 'y29UC29Szq', 'Aw5KzxGUAhrTBa', 'BxnN', 'y3rPB25Zl2DLDa', 'wKjizhvWCJzw', 'CI1PBNb1Da', 'vxnLCKrHDge/BG', 'AwXLihrYEwLUzW', 'AhjLzG', 'ntCZntKZmZDnAuP4C0W', 'yxbWBhK', 'DxnLCIbKyxrHlG', 'ANnVBG', 'C2nVCMqUz2CVAG', 'odG0mtqXr0vyrwfR', 'CM4GDgHPCYiPka', 'D2fYBG', 'kcGOlISPkYKRkq', 'C3rYAw5NAwz5', 'yMLUza', 'DgfIBgu', 'Awz5lMfWCc8UBG', 'Bg9NAw4TyNv0Da', 'Dg9tDhjPBMC', 'x19WCM90B19F', 'otm4mJKUBMv0Ba', 'y2f0y2G', 'ChjVDg90ExbL', 'mJi2mZaXng1XsuPlDa', 'mteWotaYnZrNCNn3twW', 'odK5mZm5nuHKtgvUBq', 'zxrSAwz5l2z1BG', 'y2n1CNjLzcb3Aa', 'z2v0rwXLBwvUDa', 'ndm1mdeWogXmtuvNEG', 'E30Uy29UC3rYDq'];
+  _0x1ddd = function () {
+    return _0x31f18e;
+  };
+  return _0x1ddd();
+}
+function _0x510c03(_0x1c54e7, _0x366258, _0x554cd1, _0x521baf) {
+  return _0x5ec3(_0x554cd1 + 0x13c, _0x521baf);
+}
+const _0x16d670 = function () {
+  let _0x2f7c3d = true;
+  return function (_0x5b68e3, _0x51447a) {
+    const _0x58bcef = _0x2f7c3d ? function () {
+      if (_0x51447a) {
+        const _0x367bc3 = _0x51447a.apply(_0x5b68e3, arguments);
+        _0x51447a = null;
+        return _0x367bc3;
+      }
+    } : function () {};
+    _0x2f7c3d = false;
+    return _0x58bcef;
+  };
+}();
+function _0x5ec3(_0x14c1a1, _0x4a367c) {
+  const _0x4a6f23 = _0x1ddd();
+  _0x5ec3 = function (_0xe0fb6a, _0x9737d4) {
+    _0xe0fb6a = _0xe0fb6a - 371;
+    let _0xd6acd = _0x4a6f23[_0xe0fb6a];
+    if (_0x5ec3.TUQIcV === undefined) {
+      var _0x380c0f = function (_0x79f30a) {
+        let _0x125034 = '';
+        let _0x1baf65 = '';
+        let _0x5e9777 = _0x125034 + _0x380c0f;
+        let _0x1194d0 = 0;
+        let _0x47ba0b;
+        let _0x57c6e0;
+        for (let _0x10aec3 = 0; _0x57c6e0 = _0x79f30a.charAt(_0x10aec3++); ~_0x57c6e0 && (_0x47ba0b = _0x1194d0 % 4 ? _0x47ba0b * 64 + _0x57c6e0 : _0x57c6e0, _0x1194d0++ % 4) ? _0x125034 += _0x5e9777.charCodeAt(_0x10aec3 + 10) - 10 !== 0 ? String.fromCharCode(255 & _0x47ba0b >> (-2 * _0x1194d0 & 6)) : _0x1194d0 : 0) {
+          _0x57c6e0 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/='.indexOf(_0x57c6e0);
         }
-    }).catch(p7 => {
-        console.error("Error:", p7);
-        alert("An error occurred while trying to fetch user data.");
-    });
+        let _0x49efb9 = 0;
+        for (let _0x373759 = _0x125034.length; _0x49efb9 < _0x373759; _0x49efb9++) {
+          _0x1baf65 += '%' + ('00' + _0x125034.charCodeAt(_0x49efb9).toString(16)).slice(-2);
+        }
+        return decodeURIComponent(_0x1baf65);
+      };
+      _0x5ec3.WBuVUD = _0x380c0f;
+      _0x14c1a1 = arguments;
+      _0x5ec3.TUQIcV = true;
+    }
+    const _0x4b3040 = _0x4a6f23[0];
+    const _0x22f39c = _0xe0fb6a + _0x4b3040;
+    const _0x485760 = _0x14c1a1[_0x22f39c];
+    if (!_0x485760) {
+      const _0x4a9617 = function (_0x279522) {
+        this.RzSFkL = _0x279522;
+        this.AUUPNY = [1, 0, 0];
+        this.KifNfw = function () {
+          return 'newState';
+        };
+        this.OzYuUP = "\\w+ *\\(\\) *{\\w+ *";
+        this.nzstQX = "['|\"].+['|\"];? *}";
+      };
+      _0x4a9617.prototype.edVCsv = function () {
+        const _0x231a9b = new RegExp(this.OzYuUP + this.nzstQX);
+        const _0x345e41 = _0x231a9b.test(this.KifNfw.toString()) ? --this.AUUPNY[1] : --this.AUUPNY[0];
+        return this.TsPusz(_0x345e41);
+      };
+      _0x4a9617.prototype.TsPusz = function (_0x751575) {
+        if (!Boolean(~_0x751575)) {
+          return _0x751575;
+        }
+        return this.xLgGtF(this.RzSFkL);
+      };
+      _0x4a9617.prototype.xLgGtF = function (_0x144be1) {
+        let _0x246499 = 0;
+        for (let _0x37c125 = this.AUUPNY.length; _0x246499 < _0x37c125; _0x246499++) {
+          this.AUUPNY.push(Math.round(Math.random()));
+          _0x37c125 = this.AUUPNY.length;
+        }
+        return _0x144be1(this.AUUPNY[0]);
+      };
+      new _0x4a9617(_0x5ec3).edVCsv();
+      _0xd6acd = _0x5ec3.WBuVUD(_0xd6acd);
+      _0x14c1a1[_0x22f39c] = _0xd6acd;
+    } else {
+      _0xd6acd = _0x485760;
+    }
+    return _0xd6acd;
+  };
+  return _0x5ec3(_0x14c1a1, _0x4a367c);
+}
+function _0x37176f(_0x511904, _0x3e6b4b, _0x1a3863, _0x2e50e7) {
+  return _0x5ec3(_0x511904 + 0x226, _0x1a3863);
+}
+const _0x5ccb34 = _0x16d670(this, function () {
+  return _0x5ccb34.toString().search("(((.+)+)+)+$").toString().constructor(_0x5ccb34).search("(((.+)+)+)+$");
+});
+_0x5ccb34();
+const _0x1ee66e = function () {
+  let _0x4ff4a5 = true;
+  return function (_0x29185f, _0x42684d) {
+    const _0x14089c = _0x4ff4a5 ? function () {
+      if (_0x42684d) {
+        const _0x3e06d6 = _0x42684d.apply(_0x29185f, arguments);
+        _0x42684d = null;
+        return _0x3e06d6;
+      }
+    } : function () {};
+    _0x4ff4a5 = false;
+    return _0x14089c;
+  };
+}();
+const _0xf24905 = _0x1ee66e(this, function () {
+  let _0x3d4395;
+  try {
+    const _0x2e758c = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x3d4395 = _0x2e758c();
+  } catch (_0x34adc1) {
+    _0x3d4395 = window;
+  }
+  const _0x3ae2dc = _0x3d4395.console = _0x3d4395.console || {};
+  const _0xb0187d = ['log', "warn", 'info', "error", 'exception', "table", "trace"];
+  for (let _0xe5a6b0 = 0; _0xe5a6b0 < _0xb0187d.length; _0xe5a6b0++) {
+    const _0x3c3e6a = _0x1ee66e.constructor.prototype.bind(_0x1ee66e);
+    const _0x2309ef = _0xb0187d[_0xe5a6b0];
+    const _0x37b8be = _0x3ae2dc[_0x2309ef] || _0x3c3e6a;
+    _0x3c3e6a.__proto__ = _0x1ee66e.bind(_0x1ee66e);
+    _0x3c3e6a.toString = _0x37b8be.toString.bind(_0x37b8be);
+    _0x3ae2dc[_0x2309ef] = _0x3c3e6a;
+  }
+});
+_0xf24905();
+document.getElementById("login-button").addEventListener('click', function () {
+  const _0x3e9808 = document.getElementById("user-number-input").value;
+  fetch("https://htmltestwebsite382938293829.netlify.app/.netlify/functions/getUserData?number=" + _0x3e9808).then(_0x39a97a => _0x39a97a.json()).then(_0x5ed2f6 => {
+    if (_0x5ed2f6 && !_0x5ed2f6.msg) {
+      localStorage.setItem("userData", JSON.stringify(_0x5ed2f6));
+      window.location.href = "index.html";
+    } else {
+      alert("Fel kod!");
+    }
+  })["catch"](_0x47e524 => {
+    console.error("Error:", _0x47e524);
+    alert("An error occurred while trying to fetch user data.");
+  });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("no-button").addEventListener("click", function () {
-        window.location.href = "https://discord.gg/private;
-    });
+  document.getElementById('no-button').addEventListener('click', function () {
+    window.location.href = "https://discord.gg/jZBHdupr6V";
+  });
 });
